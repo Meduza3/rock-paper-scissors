3c2d2916d9e490897bbfd7f275264ce764af0b5e
Meduza3 <jellyfish200325@gmail.com>
2022-10-24 18:21:03 +0200

    function getComputerChoice returns a random play

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Test!");
+let play;
+function getComputerChoice(){
+    play = Math.random()
+    if(play>2/3){
+        return("Rock");
+    } else if(play>1/3){
+        return("Paper");
+    } else {
+        return("Scissors");
+    }
+    
+}
